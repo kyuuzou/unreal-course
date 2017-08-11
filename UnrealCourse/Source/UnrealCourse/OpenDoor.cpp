423f1417b443c3ac3dc391a0273ca4e6a6c59a85
Nelson Rodrigues <nelson.rodrigues@miniclip.com>
2017-08-11 19:27:06 +0100

    Create first room.

---
+++ UnrealCourse/Source/UnrealCourse/OpenDoor.cpp
@@ -0,0 +1,37 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "UnrealCourse.h"
+#include "OpenDoor.h"
+
+
+// Sets default values for this component's properties
+UOpenDoor::UOpenDoor()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UOpenDoor::BeginPlay()
+{
+	Super::BeginPlay();
+
+    AActor* owner = this->GetOwner ();
+    
+    FRotator rotation = FRotator (0.0f, 90.0f, 0.0f);
+    owner->SetActorRotation (rotation);
+}
+
+
+// Called every frame
+void UOpenDoor::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
--- UnrealCourse/Source/UnrealCourse/PositionReporter.cpp
+++ UnrealCourse/Source/UnrealCourse/PositionReporter.cpp
@@ -3,6 +3,7 @@
 #include "UnrealCourse.h"
 #include "PositionReporter.h"
 
+#include "GameFramework/Actor.h"
 
 // Sets default values for this component's properties
 UPositionReporter::UPositionReporter()
@@ -19,9 +20,13 @@ UPositionReporter::UPositionReporter()
 void UPositionReporter::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
+    
+    AActor* owner = this->GetOwner ();
+    FString name = owner->GetName ();
+    FVector position = owner->GetActorLocation ();
+    FString alternatePosition = owner->GetTransform ().GetLocation ().ToString ();
+    UE_LOG (LogTemp, Warning, TEXT ("%s is at (%f, %f, %f)"), *name, position.X, position.Y, position.Z);
+    UE_LOG (LogTemp, Warning, TEXT ("%s is at %s"), *name, *alternatePosition);
 }
 
 
